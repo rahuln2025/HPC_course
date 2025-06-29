508d8d42d1a72e090c5569c0dc399df2ee6b2b02
Rahul Vishnu Narkhede <rn14pyry@mlogin01.cluster>
2025-06-29 23:18:20 +0200

    Strong scalability tests in progress

--- PVL_2023/hybrid_qsub_job.sh
+++ PVL_2023/hybrid_qsub_job.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #PBS -N PVL2023_hybrid
 #PBS -q teachingq
-# Request 1 node with 8 total CPUs, 1 MPI rank per node, 8 OpenMP threads
-#PBS -l select=1:ncpus=8:mpiprocs=1:mem=128gb
+#PBS -l select=2:ncpus=8:mpiprocs=8:mem=64gb
 #PBS -l walltime=00:30:00
 # Standard output / error
-#PBS -o hybrid_4000_1000.out
-#PBS -e hybrid_4000_1000.err
+#PBS -o strong_mpi_16_4000_1000.out
+#PBS -e strong_mpi_16_4000_1000.err
 
 echo "Job started from $(pwd)"
 # Switch to your working directory:
@@ -14,8 +13,9 @@ cd $HOME/PVL_2023
 echo "Running in directory $(pwd)"
 
 # Tell OpenMP how many threads to spawn per MPI rank
-export OMP_NUM_THREADS=8
+export OMP_NUM_THREADS=1
+echo "MPI Only Strong Scalability Test with 2 ranks and 16 cores"
 echo "OMP_NUM_THREADS = $OMP_NUM_THREADS"
 
-# Launch exactly one MPI rank (per select) which will internally spawn 8 threads
+# Launch 
 time mpiexec ./hybrid_4000_1000

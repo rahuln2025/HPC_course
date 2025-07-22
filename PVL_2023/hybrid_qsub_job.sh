758a7c2b132002f766015a8544d5624d838d50ca
Rahul Vishnu Narkhede <rn14pyry@mlogin01.cluster>
2025-07-22 23:34:11 +0200

    qsub jobscript updated

--- PVL_2023/hybrid_qsub_job.sh
+++ PVL_2023/hybrid_qsub_job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #PBS -N PVL2023_hybrid
 #PBS -q teachingq
-#PBS -l select=1:ncpus=4:mpiprocs=4:mem=128gb
+#PBS -l select=1:ncpus=8:mpiprocs=1
 #PBS -l walltime=00:30:00
 # Standard output / error
-#PBS -o hybrid2_4_1000_1000.out
-#PBS -e hybrid2_4_1000_1000.err
+#PBS -o weak_M1_2828_1000.out
+#PBS -e weak_M1_2828_1000.err
 
 echo "Job started from $(pwd)"
 # Switch to your working directory:
@@ -13,10 +13,10 @@ cd $HOME/PVL_2023
 echo "Running in directory $(pwd)"
 
 # Tell OpenMP how many threads to spawn per MPI rank
-export OMP_NUM_THREADS=2
-echo "select=1:ncpus=4:mpiprocs=4:mem=64gb"
-echo "Hybrid 2.0 Strong Scalability Test 2.0 with 4 ranks and 4 cores"
+export OMP_NUM_THREADS=1
+echo select=1:ncpus=8:mpiprocs=1
+echo "MPI Only 2.0 Weak Scalability Test M1 with 1 ranks and 8 cores"
 echo "OMP_NUM_THREADS = $OMP_NUM_THREADS"
 
 # Launch 
-time mpiexec -- ./hybrid_version2_1000_1000 --store
+time mpiexec -- ./weak_2828_1000
